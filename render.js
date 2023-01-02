74920a1afd20f2c4b57874d9ddcfb3ba1c33711b
Rufat Eskendarov <rufat.eskendarov@gmail.com>
2023-01-02 15:07:01 -0400

    Updated license link and section criator

--- render.js
+++ render.js
@@ -1,5 +1,46 @@
 const fs = require("fs");
+// Function that createss a license badge based on which license is passed in
+// If there is no license, return an empty string
+function createLicenseBadge(license) {
+  if (!license) {
+    return ``;
+  } else {
+    return `[![${license} license](https://img.shields.io/badge/License-${license}-blue.svg)](${createLicenseLink(
+      license
+    )})`;
+  }
+}
 
+// Function that creates the license link
+// If there is no license, return an empty string
+function createLicenseLink(license) {
+  if (license === "MIT") {
+    return `https://lbesson.mit-license.org/`;
+  }
+  if (license === "GPL") {
+    return `http://perso.crans.org/besson/LICENSE.html`;
+  }
+  if (license === "CC--0") {
+    return `https://creativecommons.org/licenses/by-nd/4.0`;
+  }
+}
+
+// Function that creates the license section of README
+// If there is no license, return an empty string
+function createLicenseSection(license) {
+  if (!license) {
+    return ``;
+  } else {
+    return `## Licenses
+      This project is covered under the ${license} license. To learn more about what this means, click the license button at the top.`;
+  }
+}
+
+//Function that creates the license
+// If there is no license, return an empty string
+function createLinkedIn() {}
+
+//Function that creates markdown for README
 function render(data) {
   return `# ${data.title}
   
@@ -21,8 +62,8 @@ function render(data) {
     ${data.usage}
     ## Credits
     ${data.credits}
-    ## License
-    ${data.license}
+    ${createLicenseSection(data.licenses)}
+    ${createLicenseBadge(data.licenses)}
     ## Contributing
     ${data.contributing}
     ## Tests
